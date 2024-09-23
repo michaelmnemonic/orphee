50ec10d205e0814b3516b2137be409647fdca006
michaelmnemonic <michaelmnemonic@posteo.eu>
2024-09-23 09:57:29 +0200

    Enable tpm unlock during boot

--- build.sh
+++ build.sh
@@ -35,6 +35,9 @@ rpm-ostree install pam_mount	\
 	libreoffice-kf6			\
 	--uninstall firewall-config
 
+# Enable tpm unlock during boot
+rpm-ostree initramfs --enable --arg=--force-add --arg=tpm2-tss
+
 # Remove .desktop file for wine applications
 rm /usr/share/applications/wine-*.desktop
 
